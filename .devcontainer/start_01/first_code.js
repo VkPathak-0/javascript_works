bda5652347ca2b34ff453613a59ba2b0d685f53e
VkPathak-0 <vkpathak2002@gmail.com>
2025-07-30 15:01:24 +0000

    changes done

---
+++ .devcontainer/start_01/first_code.js
@@ -0,0 +1,6 @@
+console.log("start of js code")
+
+// sets up nodejs environment through devcontainer configuration to install bydefault programs like nodejs or python etc.
+// adds devcontainer through command pallete search nodejs and js last click on rebuild to modify changes to the system.
+// create file and folder for our programme code start writing code
+// to save our file to our account
